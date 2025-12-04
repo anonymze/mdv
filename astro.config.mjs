0fcc9fb8a7ab8b66e1a6c4812391cd31d4a81aa8
anonymze <anodevfr@gmail.com>
2025-12-04 20:47:48 +0100

    add: all updtes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@
 import { defineConfig } from 'astro/config'
 
 import react from '@astrojs/react'
+import vercel from '@astrojs/vercel/static'
 
 import tailwindcss from '@tailwindcss/vite'
 import { imageService } from '@unpic/astro/service'
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'static',
+	adapter: vercel(),
 	integrations: [react()],
 	site: "https://mdv-chi.vercel.app",
 	i18n: {
