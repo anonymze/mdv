188884d3e4f7ed82e854ba538060f6363364985e
anonymze <anodevfr@gmail.com>
2026-02-02 23:46:51 +0100

    add: all updtes

--- src/types/cinema.ts
+++ src/types/cinema.ts
@@ -1,33 +1,33 @@
-import type { Media } from "./media";
-import type { RichTextField } from "./rich-text";
+import type { Media } from './media'
+import type { RichTextField } from './rich-text'
 
 export interface Cinema {
-  id: string;
-  type: 'seance' | 'scolaire' | 'rdv_cinema';
-  archive?: boolean | null;
-    jeunePublic?: boolean | null;
-  thumbnail?: Media | null;
-  title: string;
-  synopsis: RichTextField;
-  price: string;
-  date_start: string;
-  video: string;
-  duration: string;
-  genre: string;
-  public?:
-    | ('children' | 'all_public' | 'all_public_avertissment' | 'forbidden_12' | 'forbidden_16' | 'forbidden_18')
-    | null;
-  languages: string;
-  informations_more?: RichTextField | null;
-  other_images?:
-    | {
-        image?: Media | null;
-        id?: string | null;
-      }[]
-    | null;
-  actors?: string | null;
-  directors?: string | null;
-  location?: string | null;
-  updatedAt: string;
-  createdAt: string;
+	id: string
+	type: 'seance' | 'scolaire' | 'rdv_cinema'
+	archive?: boolean | null
+	jeunePublic?: boolean | null
+	thumbnail?: Media | null
+	title: string
+	synopsis: RichTextField
+	price: string
+	date_start: string
+	video: string
+	duration: string
+	genre: string
+	public?:
+		| ('children' | 'all_public' | 'all_public_avertissment' | 'forbidden_12' | 'forbidden_16' | 'forbidden_18')
+		| null
+	languages: string
+	informations_more?: RichTextField | null
+	other_images?:
+		| {
+				image?: Media | null
+				id?: string | null
+		  }[]
+		| null
+	actors?: string | null
+	directors?: string | null
+	location?: string | null
+	updatedAt: string
+	createdAt: string
 }
--- src/types/mediatheque.ts
+++ src/types/mediatheque.ts
@@ -3,17 +3,11 @@ import type { RichTextField } from "./rich-text";
 
 export interface Mediatheque {
   id: string;
-  type: 'bibliotheque' | 'ludotheque';
-  archive?: boolean | null;
-  /**
-   * Le fichier doit être une image.
-   */
+  type: 'ludotheque';
+  jeunePublic?: boolean | null;
   thumbnail?: Media | null;
   title: string;
   description: RichTextField;
-  /**
-   * Séparer si plusieurs prix par un ";" Ex : 5€ adulte; Gratuit sur place; 2€ par jour...
-   */
   price: string;
   players: '1' | '1-2' | '1-4' | '1-8' | '4+';
   duration: '15' | '30' | '1h' | '1h+';
@@ -21,29 +15,14 @@ export interface Mediatheque {
   informations_more?: RichTextField | null;
   other_images?:
     | {
-        /**
-         * Le fichier doit être une image.
-         */
         image?: Media | null;
         id?: string | null;
       }[]
     | null;
-  /**
-   * Ex : Du 1er janvier 2026 au 31 juillet 2026
-   */
   availability_date?: string | null;
-  /**
-   * Ex : Logique, Déduction, Coopération
-   */
   genre?: string | null;
-  /**
-   * Ex : 27 tuiles enquête, 10 petits punchs, 1 feuillet de règles
-   */
   materiel?: string | null;
   authors?: string | null;
-  /**
-   * SI VOUS LAISSEZ CE CHAMP VIDE, LE LIEU PAR DÉFAULT SERA À LA MÉDIATHÈQUE DE LA MDV
-   */
   location?: string | null;
   updatedAt: string;
   createdAt: string;
