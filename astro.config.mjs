babd793f212fd6d3528fc1cea12240bfd76ecebc
anonymze <anodevfr@gmail.com>
2026-06-21 14:55:38 +0200

    STATIC

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
+    output: 'static',
     adapter: vercel({
         imageService: true,
         imagesConfig: {
